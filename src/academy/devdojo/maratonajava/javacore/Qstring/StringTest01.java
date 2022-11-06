aa28fd4e2e8cdaa487024c3f696d8e274682e9ba
alessandroF-dev <alessandro.dev01@gmail.com>
2022-11-06 16:52:47 -0300

    codigo organizado

--- src/academy/devdojo/maratonajava/javacore/Qstring/StringTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/StringTest01.java
@@ -4,9 +4,12 @@ public class StringTest01 {
     public static void main(String[] args) {
         String nome = "Alessandro"; //String constant pool
         String nome2 = "Alessandro";
+        String nome3 = new String("Alessandro");
         nome = nome.concat(" Fernandes");
 
         System.out.println(nome);
         System.out.println(nome == nome2);
+        System.out.println(nome2 == nome3);
+
     }
 }
