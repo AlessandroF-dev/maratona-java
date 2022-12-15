3bd88ea3df9da3fd10ac1080d9035e7a7abadad6
alessandroF-dev <alessandro.dev01@gmail.com>
2022-12-15 19:11:08 -0300

    adicionado novas aulas

---
+++ src/academy/devdojo/maratonajava/Xserializacao/SerializacaoTest01.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.Xserializacao;
+
+import academy.devdojo.maratonajava.Xserializacao.dominio.Aluno;
+import academy.devdojo.maratonajava.Xserializacao.dominio.Turma;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SerializacaoTest01 {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno(12L, "Alessandro", "1234");
+        Turma turma = new Turma("Maratona Java Virado no Jiraya");
+        aluno.setTurma(turma);
+        serializar(aluno);
+        deserializar();
+    }
+
+    private static void serializar (Aluno aluno){
+        Path path = Paths.get("pasta/aluno.ser");
+        try(ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))){
+            oos.writeObject(aluno);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    private static void deserializar (){
+        Path path = Paths.get("pasta/aluno.ser");
+        try(ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))){
+            Aluno aluno = (Aluno) ois.readObject();
+            System.out.println(aluno);
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/academy/devdojo/maratonajava/Xserializacao/dominio/Turma.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.Xserializacao.dominio;
+
+public class Turma {
+    private String nome;
+
+    @Override
+    public String toString() {
+        return "Turma{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+
+    public Turma(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
