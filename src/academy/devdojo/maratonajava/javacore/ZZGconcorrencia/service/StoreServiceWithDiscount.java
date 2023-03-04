576f765fd974f7744e7da585007fd1dff6007419
alessandroF-dev <alessandro.dev01@gmail.com>
2023-03-04 13:38:12 -0300

    adicionado novas aulas

--- src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/service/StoreServiceWithDiscount.java
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/service/StoreServiceWithDiscount.java
@@ -1,12 +1,28 @@
 package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.service;
 
+import academy.devdojo.maratonajava.javacore.ZZGconcorrencia.dominio.Discount;
+import academy.devdojo.maratonajava.javacore.ZZGconcorrencia.dominio.Quote;
+
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 public class StoreServiceWithDiscount {
-    public double getPriceSync(String storeName) {
-        System.out.printf("Getting prices sync for store %s%n", storeName);
-        return priceGenerator();
+    public String getPriceSync(String storeName) {
+        double price = priceGenerator();
+        Discount.Code discountCode = Discount.Code.values()[
+                ThreadLocalRandom.current().nextInt(Discount.Code.values().length)
+                ];
+        return String.format("%s:%.2f:%s", storeName, price,discountCode);
+    }
+
+    public String applyDiscount (Quote quote){
+        delay();
+        double discountValue = quote.getPrice() * (100 - quote.getDiscountCode().getPercentage()) / 100;
+        return String.format("'%s' original price: '%.2f'. Applying discount code '%s'. Final price: '%.2f'",
+                quote.getStore(),
+                quote.getPrice(),
+                quote.getDiscountCode(),
+                discountValue);
     }
 
     private double priceGenerator() {
@@ -16,7 +32,8 @@ public class StoreServiceWithDiscount {
 
     private void delay() {
         try {
-            TimeUnit.SECONDS.sleep(1);
+            int milli = ThreadLocalRandom.current().nextInt(200, 2500);
+            TimeUnit.SECONDS.sleep(milli);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
